5b5eb11e9ca5e524ac6bfcf27e8391142b20ef4e
Jang-Hyeok00 <puswkdgur3030@gmail.com>
2025-08-27 19:40:17 +0900

    commit message

---
+++ src/main/Main.java
@@ -0,0 +1,28 @@
+package main;
+
+import javax.swing.JFrame;
+
+//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
+// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+public class Main extends JFrame {
+
+    public static void main(String[] args) {
+
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("2D adventure");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+
+        gamePanel.setUpGame();
+        gamePanel.startGameThread();
+
+    }
+}
